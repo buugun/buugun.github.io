e23c501604b20d7ded44cc28b5a8bf6d6f495d1a
caijinlin <1399534656@qq.com>
2014-06-18 20:21:40 +0800

    my commit message

--- scripts/browserCheck.js
+++
@@ -1,7 +0,0 @@
-// JavaScript Document
-window.addEvent('domready',function(e){
-	if(Browser.Engine.name=='webkit'){
-		$('warning').setStyle('display', 'none');
-		$('msg').setStyle('display', 'block');
-	}
-});
